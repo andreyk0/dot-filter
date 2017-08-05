34afccd00aabeba6b4bb11e96fa8f5988198a035
Andrey Kartashov <akartashov@gilt.com>
2017-08-05 13:55:30 -0400

    Filter simple dot graphs.
    Find paths matching a keyword.

--- Args.hs
+++ Args.hs
@@ -10,44 +10,25 @@ module Args (
 
 import           Control.Monad
 import           Data.Monoid
+import           Data.Text.Lazy (Text)
+import qualified Data.Text.Lazy as T
 import           Development.GitRev
 import           Options.Applicative
 import           System.Exit
 
 
-data Custom = Custom1 | Custom2 deriving (Eq, Show, Ord)
-
-
-data Args = Args { argsVerbose :: !Bool
-                 , argsVersion :: !Bool
-                 , argsCustom :: !Custom
+data Args = Args { argsVersion :: !Bool
+                 , argsKeywords :: ![Text]
                  } deriving (Show)
 
 
 parseArgs :: Parser Args
 parseArgs = Args
      <$> switch
-         ( long "verbose"
-        <> short 'v'
-        <> help "Be verbose.")
-     <*> switch
          ( long "version"
         <> short 'V'
         <> help "Print version and exit.")
-     <*> option parseCustomArg
-         ( long "custom-arg"
-        <> short 'c'
-        <> value Custom1
-        <> showDefault
-        <> help "Arg with a custom parser." )
-
-
-parseCustomArg :: ReadM Custom
-parseCustomArg = eitherReader $ \s ->
-  case s
-    of "c1" -> Right Custom1
-       "c2" -> Right Custom2
-       x    -> Left $ "Failed to parse custom argument " <> x <> ", expected 'c1' or 'c2'"
+     <*> some (T.pack <$> argument str (metavar "keyword"))
 
 
 runWithArgs :: (Args -> IO ())
@@ -56,8 +37,8 @@ runWithArgs rwa = execParser opts >>= printVersion >>= rwa
   where
     opts = info (helper <*> parseArgs)
       ( fullDesc
-     <> header "Add program header here."
-     <> progDesc "Add program description here. Source: https://github.com/andreyk0/dot-filter")
+     <> header "Tiny hack to filter simple dot graphs to find nodes/edges matching a keyword."
+     <> progDesc "E.g. $ cat foo.dot | dot-filter keyword > bar.dot")
 
     printVersion args@Args{..} = do
       when argsVersion $ die $ "Version: " <> $(gitBranch) <> "@" <> $(gitHash)
--- Main.hs
+++ Main.hs
@@ -1,10 +1,55 @@
-{-# LANGUAGE BangPatterns     #-}
-{-# LANGUAGE RecordWildCards  #-}
+{-# LANGUAGE OverloadedStrings    #-}
+{-# LANGUAGE RecordWildCards      #-}
 
 module Main where
 
 import           Args
+import           Data.GraphViz
+import           Data.GraphViz.Printing
+import qualified Data.GraphViz.Types.Generalised as G
+import           Data.List (find, partition)
+import           Data.Maybe
+import           Data.Sequence ((><))
+import qualified Data.Sequence as Seq
+import           Data.Set (Set)
+import qualified Data.Set as Set
+import           Data.Text.Lazy (Text)
+import qualified Data.Text.Lazy as T
+import qualified Data.Text.Lazy.IO as L
+
 
 main:: IO ()
-main = runWithArgs $ \args@Args{..} -> do
-  putStrLn $ show args
+main = runWithArgs $ \Args{..} -> do
+  xDotText <- L.getContents
+  let xDotGraph = parseDotGraphLiberally xDotText :: G.DotGraph Text
+
+  let matchNodeId nid = isJust $ find (`T.isInfixOf` nid) argsKeywords
+
+  let newEdges = filterEdges matchNodeId Set.empty [] (graphEdges xDotGraph)
+      newNodeIds = Set.fromList $ (fromNode <$> newEdges) ++ (toNode <$> newEdges)
+      newNodes = filter (\DotNode{..} -> Set.member nodeID newNodeIds) $ graphNodes xDotGraph
+
+  L.putStrLn $ renderDot $ toDot $ xDotGraph {
+    G.graphStatements =
+        Seq.fromList (G.DE <$> newEdges) ><
+        Seq.fromList (G.DN <$> newNodes)
+  }
+
+
+filterEdges:: (Text -> Bool) -- ^ matches nodes by ID
+           -> Set Text -- ^ vertices to keep
+           -> [DotEdge Text] -- ^ edges to keep
+           -> [DotEdge Text] -- ^ edges to process
+           -> [DotEdge Text]
+filterEdges matchNodeId vs es es' =
+  let (m,nm) = partition isMatchingEdge es'
+   in if null m
+      then es
+      else filterEdges
+             matchNodeId
+             (Set.union vs (Set.fromList ((toNode <$> m) ++ (fromNode <$> m))))
+             (es ++ m)
+             nm
+
+  where isMatchingEdge DotEdge{..} =
+          matchNodeId toNode || Set.member toNode vs
